fcedfa7b86f44619e5220135449bcc16bf358e3c
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-14 00:56:26 +0300

    modified : account controller

--- src/main/java/eg/boe/banqueofegypt/controller/AccountController.java
+++ src/main/java/eg/boe/banqueofegypt/controller/AccountController.java
@@ -7,8 +7,8 @@ import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Setter
 @Getter
@@ -17,17 +17,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AccountController {
     private final AccountService accountService;
 
-    //todo replace return value to page name
-    @PostMapping("/addAccount")
-    public String addAccount(@RequestBody AccountDto accountDto) {
-        accountService.addAccount(accountDto);
-        return "redirect:/";
+    @GetMapping("/accounts")
+    public String accounts(Model model) {
+        model.addAttribute("account", new AccountDto());
+        return "registration";
     }
-
     //todo replace return value to page name
     @GetMapping("/getAllAccounts")
-    public String getAllAccounts(Model model) {
-        model.addAttribute("accounts", accountService.getAllAccounts());
+    public String getAllAccounts() {
         return "accounts";
+    }
+    @PostMapping("/createAccount")
+    public String createAccount(@ModelAttribute("account") AccountDto account , Model model) {
+        accountService.addAccount(account);
+        model.addAttribute("accounts", accountService.getAllAccounts());
+        return "registration";
     }
 }
