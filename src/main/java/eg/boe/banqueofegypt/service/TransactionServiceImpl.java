f6f2db6c35c7e5cb34d823bc18cecb2ccc77eaeb
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-13 15:03:48 +0300

    added TransactionDto

--- src/main/java/eg/boe/banqueofegypt/service/TransactionServiceImpl.java
+++ src/main/java/eg/boe/banqueofegypt/service/TransactionServiceImpl.java
@@ -2,8 +2,11 @@ package eg.boe.banqueofegypt.service;
 
 import eg.boe.banqueofegypt.controller.TransactionService;
 import eg.boe.banqueofegypt.db.TransactionRepository;
+import eg.boe.banqueofegypt.entity.Transaction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Repository
@@ -11,4 +14,8 @@ public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
 
 
+    @Override
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
 }
