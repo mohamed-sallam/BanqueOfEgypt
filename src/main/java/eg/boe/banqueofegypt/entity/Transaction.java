c54e99736d87a096412f78fb5e32c39cc5e8b533
Medhat Mohammed <medhatmohammed1612@gmail.com>
2024-08-13 18:09:04 +0300

    config/config: Add validation to Entities

--- src/main/java/eg/boe/banqueofegypt/entity/Transaction.java
+++ src/main/java/eg/boe/banqueofegypt/entity/Transaction.java
@@ -1,6 +1,7 @@
 package eg.boe.banqueofegypt.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,15 +19,30 @@ import java.util.Map;
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Pattern(regexp = "^[0-9]*$", message = "Id must be a number")
     private Long id;
+
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "payer_id")
+    @NotNull(message = "Payer is required")
     private Account payer;
+
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "payee_id")
+    @NotNull(message = "Payee is required")
     private Account payee;
+
+    @Pattern(regexp = "^[0-9]*$", message = "Amount must be a number")
+    @Positive(message = "Amount must be not negative or zero")
+    @Size(min = 4, max = 9, message = "amounts must be from $10,000 to $1000,000,000")
     private String amount;
+
+    @NotNull(message = "Status is required")
+    @Enumerated(EnumType.STRING)
     private Status status;
+
+    @NotNull(message = "Date is required")
+    @PastOrPresent(message = "Date must be in the past or present")
     private Date date;
 
     public enum Status {
