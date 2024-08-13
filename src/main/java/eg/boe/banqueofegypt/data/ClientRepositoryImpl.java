99b644f0b9c7be19a1c4005fe8aeb0283a9dab75
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-13 20:34:05 +0300

    modified ClientRepositoryImpl

--- src/main/java/eg/boe/banqueofegypt/data/ClientRepositoryImpl.java
+++ src/main/java/eg/boe/banqueofegypt/data/ClientRepositoryImpl.java
@@ -1,33 +1,38 @@
 package eg.boe.banqueofegypt.data;
 
 import eg.boe.banqueofegypt.data.Request.CheckBalanceRequest;
-import eg.boe.banqueofegypt.data.Response.BalanceResponse;
 import eg.boe.banqueofegypt.data.Response.Response;
 import lombok.AllArgsConstructor;
 import org.springframework.web.client.RestClient;
+
 @AllArgsConstructor
 public class ClientRepositoryImpl {
     RestClient restClient = RestClient.create();
-    Response<?> checkBalance (CheckBalanceRequest request,String url){
-       return restClient
+
+    Response<?> checkBalance(CheckBalanceRequest request, String url) {
+        return restClient
                 .post()
-                .uri(url+"/balance")
+                .uri(url + "/balance")
+                .body(request)
                 .retrieve()
                 .body(Response.class);
     }
 
-    Response<?> depositMoney (CheckBalanceRequest request,String url){
+    Response<?> depositMoney(CheckBalanceRequest request, String url) {
         return restClient
-               .post()
-               .uri(url+"/deposit")
-               .retrieve()
-               .body(Response.class);
+                .post()
+                .uri(url + "/deposit")
+                .body(request)
+                .retrieve()
+                .body(Response.class);
     }
-    Response<?> withdrawMoney (CheckBalanceRequest request,String url){
+
+    Response<?> withdrawMoney(CheckBalanceRequest request, String url) {
         return restClient
-               .post()
-               .uri(url+"/withdraw")
-               .retrieve()
-               .body(Response.class);
+                .post()
+                .uri(url + "/withdraw")
+                .body(request)
+                .retrieve()
+                .body(Response.class);
     }
 }
