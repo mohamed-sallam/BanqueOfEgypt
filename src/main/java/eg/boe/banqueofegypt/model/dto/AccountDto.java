6cb0d514ed65ae9c94ff326d2e12cbdcf7faf6e6
Medhat Mohammed <medhatmohammed1612@gmail.com>
2024-08-13 17:03:32 +0300

    config/config: fix typo

--- src/main/java/eg/boe/banqueofegypt/model/dto/AccountDto.java
+++ src/main/java/eg/boe/banqueofegypt/model/dto/AccountDto.java
@@ -1,5 +1,12 @@
 package eg.boe.banqueofegypt.model.dto;
 
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountDto {
     private Long id;
     private String name;
