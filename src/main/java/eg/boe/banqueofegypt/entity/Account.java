2cc740ed17ff4ea02bfca81b2112a3c1bd5f1285
VEX <ahmedsherif000035@gmail.com>
2024-08-13 14:28:07 +0300

    Add account entity

--- src/main/java/eg/boe/banqueofegypt/entity/Account.java
+++ src/main/java/eg/boe/banqueofegypt/entity/Account.java
@@ -1,4 +1,31 @@
 package eg.boe.banqueofegypt.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Account")
 public class Account {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "swift_code")
+    private String swiftCode;
+    @Column(name = "balance")
+    private String balance;
+    //todo @OneToMany(mappedBy = "account")
+    //todo @JoinColumn(name = " ")
+    //todo private List<Transaction> transactionList;
+
 }
