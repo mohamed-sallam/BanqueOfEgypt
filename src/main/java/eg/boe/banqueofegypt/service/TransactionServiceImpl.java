27c00a984602fea35f6e36051d7688f23b8a8ba9
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-13 15:07:45 +0300

    added impl of getAllTransactions service

--- src/main/java/eg/boe/banqueofegypt/service/TransactionServiceImpl.java
+++ src/main/java/eg/boe/banqueofegypt/service/TransactionServiceImpl.java
@@ -3,7 +3,9 @@ package eg.boe.banqueofegypt.service;
 import eg.boe.banqueofegypt.controller.TransactionService;
 import eg.boe.banqueofegypt.db.TransactionRepository;
 import eg.boe.banqueofegypt.entity.Transaction;
+import eg.boe.banqueofegypt.model.dto.TransactionDto;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,10 +14,13 @@ import java.util.List;
 @Repository
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
-    public List<Transaction> getAllTransactions() {
-        return transactionRepository.findAll();
+    public List<TransactionDto> getAllTransactions() {
+        return transactionRepository.findAll().stream().map(it->
+                modelMapper.map(it, TransactionDto.class)
+        ).toList();
     }
 }
