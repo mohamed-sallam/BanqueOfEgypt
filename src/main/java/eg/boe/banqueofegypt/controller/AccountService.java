46cc56743280a4a293ac943046ee24f8f58f90d6
VEX <ahmedsherif000035@gmail.com>
2024-08-13 14:49:04 +0300

    Add AccountService Implementation and interface.

--- src/main/java/eg/boe/banqueofegypt/controller/AccountService.java
+++ src/main/java/eg/boe/banqueofegypt/controller/AccountService.java
@@ -1,4 +1,10 @@
 package eg.boe.banqueofegypt.controller;
 
+import eg.boe.banqueofegypt.model.dto.AccountDto;
+
+import java.util.List;
+
 public interface AccountService {
+    List<AccountDto> getAllAccounts();
+    AccountDto addAccount(AccountDto accountDto);
 }
