062ad079cec88572c376954302d5043e3ee2de01
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2024-08-14 16:58:20 +0300

    Fix balance feature

--- src/main/java/eg/boe/banqueofegypt/service/AccountServiceImpl.java
+++ src/main/java/eg/boe/banqueofegypt/service/AccountServiceImpl.java
@@ -1,6 +1,9 @@
 package eg.boe.banqueofegypt.service;
 
 import eg.boe.banqueofegypt.controller.AccountService;
+import eg.boe.banqueofegypt.controller.ClientService;
+import eg.boe.banqueofegypt.data.dto.BalanceResponse;
+import eg.boe.banqueofegypt.data.dto.CheckBalanceRequest;
 import eg.boe.banqueofegypt.entity.Account;
 import eg.boe.banqueofegypt.model.dto.AccountDto;
 import lombok.RequiredArgsConstructor;
@@ -9,18 +12,24 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static eg.boe.banqueofegypt.service.TransactionServiceImpl.TOKEN;
+
 @RequiredArgsConstructor
 @Service
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
+    private final ClientService clientService;
     private final ModelMapper modelMapper;
 
     @Override
     public List<AccountDto> getAllAccounts() {
-        return accountRepository.
-                findAll().
-                stream().
-                map(s -> modelMapper.map(s,AccountDto.class)).toList();
+        List<AccountDto> accounts = accountRepository.findAll().stream().map(s -> modelMapper.map(s, AccountDto.class)).toList();
+
+        return accounts.stream().map(acc -> {
+            BalanceResponse balanceResponse = clientService.getBalance(new CheckBalanceRequest(TOKEN), acc.getUrl());
+            acc.setBalance(balanceResponse.getBalance());
+            return acc;
+        }).toList();
     }
 
     @Override
