1a2457340a91fabf772d329f5ecad8afd55b0c7d
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-08-14 09:49:56 +0300

    Modified : TransactionController

--- src/main/java/eg/boe/banqueofegypt/controller/TransactionController.java
+++ src/main/java/eg/boe/banqueofegypt/controller/TransactionController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Setter
 @Getter
@@ -17,19 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class TransactionController {
     private final TransactionService transactionService;
 
-    @GetMapping("/transactions")
-    public String getAllTransaction(Model model) {
-        model.addAttribute("transactions", transactionService.getAllTransactions());
-        //TODO REPLACE WITH CORRECT HTML FILE
-        return "transactions";
-    }
 
-    @PostMapping
+
+    @GetMapping()
     public String getAllTransaction(@ModelAttribute TransactionRetrievalDto transaction, Model model) {
-        //TODO DO Transaction
         model.addAttribute("transactions", transactionService.getAllTransactions());
-        //TODO REPLACE WITH CORRECT HTML FILE
-        return "transactions";
+        return "registration";
     }
 
 }
